6f03df83d67cc14a841df2b1bb650a3277a3722a
agriemporiodeanna <shop@agriemporiodeanna.com>
2025-12-30 19:31:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, send_from_directory, jsonify
 import traceback
-import scripts.sync_articoli as sync_articoli
 import scripts.test_google as test_google
 import scripts.test_bman as test_bman
+import scripts.sync_articoli as sync_articoli
+import scripts.sync_anagrafica as sync_anagrafica
 
 app = Flask(__name__)
 
+# Serve la dashboard sulla root e su /dashboard.html
+@app.route('/')
 @app.route('/dashboard.html')
 def dashboard():
     return send_from_directory('static', 'dashboard.html')
@@ -20,16 +23,21 @@ def handle_api(action):
             msg = test_bman.run()
         elif action == 'sync-articoli':
             msg = sync_articoli.run()
+        elif action == 'sync-anagrafica':
+            msg = sync_anagrafica.run()
         else:
             return jsonify({"status": "error", "message": "Azione non valida"}), 404
         
         return jsonify({"status": "success", "message": msg})
     
     except Exception as e:
-        # Recupera l'errore completo con i dettagli di Google
         full_error = traceback.format_exc()
         print(f"ERRORE DETTAGLIATO:\n{full_error}")
-        return jsonify({"status": "error", "message": f"Errore: {str(e)}", "details": full_error}), 500
+        return jsonify({
+            "status": "error", 
+            "message": f"Errore: {str(e)}", 
+            "details": full_error
+        }), 500
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
